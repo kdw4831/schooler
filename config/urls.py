13d939147d125894ccb65b611777faecaae4b5ab
Deawon <19999eo@naver.com>
2023-06-09 20:03:32 +0900

    add

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pybo.urls')),
-    path('question/create/', views.question_create, name='question_create'),
+    path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]
